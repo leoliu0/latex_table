b8d009d8e0651702cc64a01c72ab07d2737648e6
Leo Liu <auleo.liu@gmail.com>
2019-04-25 10:42:46 +1000

    setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="latex_table_leoliu",
+    version="0.0.1",
+    author="Leo Liu",
+    author_email="auleo.liu@example.com",
+    description="create latex table from adding rows",
+    long_description_content_type="text/markdown",
+    url="https://github.com/leoliu0/latex_table",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
